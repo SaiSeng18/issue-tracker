19bbb29e0e658d579c477d47e76e3d64720c9dab
SaiSeng18 <saisengmongli14@gmail.com>
2023-10-19 22:12:54 +0630

    first commit

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,11 +1,18 @@
+"use client";
+
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import { AiFillBug } from "react-icons/ai";
+import classnames from "classnames";
 
 const NavBar = () => {
 	const links = [
 		{ label: "Dashboard", href: "/" },
 		{ label: "Issues", href: "/issues" },
 	];
+
+	const currentPath = usePathname();
+
 	return (
 		<nav className="flex space-x-6 border-b mb-5 px-5 h-14 items-center">
 			<Link href="/">
@@ -17,7 +24,11 @@ const NavBar = () => {
 					<Link
 						key={link.href}
 						href={link.href}
-						className="text-zinc-500 hover:text-zinc-800 transition-colors">
+						className={classnames({
+							"text-zinc-900": link.href === currentPath,
+							"text-zinc-500": link.href !== currentPath,
+							"hover:text-zinc-800 transition-colors": true,
+						})}>
 						{link.label}
 					</Link>
 				))}
---
+++ app/issues/new/page.tsx
@@ -0,0 +1,73 @@
+"use client";
+
+import { Button, Callout, Text, TextField } from "@radix-ui/themes";
+import SimpleMDE from "react-simplemde-editor";
+import "easymde/dist/easymde.min.css";
+import { useForm, Controller } from "react-hook-form";
+import axios from "axios";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { createIssueSchema } from "@/app/validationSchema";
+import z from "zod";
+
+type IssueForm = z.infer<typeof createIssueSchema>;
+
+// interface IssueForm {
+// 	title: string;
+// 	description: string;
+// }
+
+const NewIssuePage = () => {
+	const {
+		register,
+		control,
+		handleSubmit,
+		formState: { errors },
+	} = useForm<IssueForm>({
+		resolver: zodResolver(createIssueSchema),
+	});
+	const router = useRouter();
+	const [error, setError] = useState("");
+
+	return (
+		<div className="max-w-xl">
+			{error && (
+				<Callout.Root color="red" className="mb-5">
+					<Callout.Text>{error}</Callout.Text>
+				</Callout.Root>
+			)}
+			<form
+				className="space-y-3"
+				onSubmit={handleSubmit(async (data) => {
+					try {
+						await axios.post("/api/issues", data);
+						router.push("/issues");
+					} catch (error) {
+						setError("An unexpected error occurred.");
+					}
+				})}>
+				<TextField.Root>
+					<TextField.Input placeholder="Title" {...register("title")} />
+				</TextField.Root>
+				{errors.title && (
+					<Text color="red" as="p">
+						{errors.title.message}
+					</Text>
+				)}
+				<Controller
+					name="description"
+					control={control}
+					render={({ field }) => <SimpleMDE placeholder="Description" {...field} />}
+				/>
+				{errors.description && (
+					<Text color="red" as="p">
+						{errors.description.message}
+					</Text>
+				)}
+				<Button>Submit New Issue</Button>
+			</form>
+		</div>
+	);
+};
+export default NewIssuePage;
