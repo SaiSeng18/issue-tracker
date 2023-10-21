05773915c942f00056d894459cbf44349ef4cf77
SaiSeng18 <saisengmongli14@gmail.com>
2023-10-21 12:01:27 +0630

    markdowns and single page view

---
+++ app/issues/[id]/page.tsx
@@ -0,0 +1,37 @@
+import IssueStatusBadge from "@/app/components/IssueStatusBadge";
+import prisma from "@/prisma/client";
+import { Card, Flex, Heading, Text } from "@radix-ui/themes";
+import { notFound } from "next/navigation";
+import ReactMarkDown from "react-markdown";
+
+interface Props {
+	params: {
+		id: string;
+	};
+}
+
+const IssueDetailPage = async ({ params: { id } }: Props) => {
+	const issue = await prisma.issue.findUnique({
+		where: {
+			id: parseInt(id),
+		},
+	});
+
+	if (!issue) {
+		notFound();
+	}
+
+	return (
+		<div>
+			<Heading>{issue.title}</Heading>
+			<Flex gap="3" my="2">
+				<IssueStatusBadge status={issue.status} />
+				<Text>{issue.createdAt.toDateString()}</Text>
+			</Flex>
+			<Card className="prose" mt="4">
+				<ReactMarkDown>{issue.description}</ReactMarkDown>
+			</Card>
+		</div>
+	);
+};
+export default IssueDetailPage;
